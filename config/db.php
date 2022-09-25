e8087a6e63c89eec9a2cdfd40f0651b97a49e1fc
Roosevelt Javier Jaramillo Arias <rusby09@hotmail.com>
2022-09-24 20:31:05 -0500

    cambio de base de datos en servidor

--- config/db.php
+++ config/db.php
@@ -1,7 +1,22 @@
 <?php
-/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '1234');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'simple_invoice');//Nombre de la base de datos
+
+/*Datos de conexion a la base de datos*///LOCAL
+/*
 define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 define('DB_USER', 'root');//Usuario de tu base de datos
 define('DB_PASS', '');//Contraseña del usuario de la base de datos
 define('DB_NAME', 'making_fact');//Nombre de la base de datos
+*/
+
+/*Datos de conexion a la base de datos*///SERVIDOR
+/*
+define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '1234');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'simple_invoice');//Nombre de la base de datos
+*/
 ?>
