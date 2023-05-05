2e12318671c2839cf6cf5f557f53d290467c0f94
Roosevelt Javier Jaramillo Arias <rusby09@hotmail.com>
2023-05-05 12:18:05 -0500

    actualizacion

--- navbar.php
+++ navbar.php
@@ -1,4 +1,4 @@
-	<?php echo "Prueba desde GIT numero 2 para confirmar gitbash";
+	<?php echo "Prueba desde GIT rosevelt";
   /* Connect To Database*/
   require_once ("config/db.php");
   require_once ("config/conexion.php");
